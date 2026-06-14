09cd365056bf6745da0da556940b0e610cab21c0
Joao Paulo <jotape.hin@gmail.com>
2026-06-14 20:01:58 -0300

    Implementa dicionario de tipos e impressão de categoria

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import time, os
 
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
-from colorama import Fore, init
+from colorama import Fore, Style, init
 
 init(autoreset=True)
 
 class monitora_arquivos(FileSystemEventHandler):
     def on_created(self, event):
         nome_arquivo = os.path.basename(event.src_path)
-        tipo_arquivo = os.path.splitext(nome_arquivo)
-        print(f'Arquivo adicionado: {Fore.YELLOW + nome_arquivo}, extensão: {Fore.YELLOW + tipo_arquivo[1]}')
+        nome, extensao = os.path.splitext(nome_arquivo)
+        categoria = tipo_arquivo[extensao]
+        print(f'Arquivo adicionado: {Fore.YELLOW + nome_arquivo + Style.RESET_ALL}. Extensão: {Fore.YELLOW + extensao + Style.RESET_ALL}, categoria: {Fore.YELLOW + categoria + Style.RESET_ALL}')
 
 observador = Observer()
 monitor = monitora_arquivos()
@@ -18,6 +19,19 @@ monitor = monitora_arquivos()
 observador.schedule(monitor, r'C:\Users\jotap\OneDrive\Área de Trabalho\Arquivos', recursive=False)
         
 observador.start()
+
+tipo_arquivo = {
+    '.txt': 'Documentos',
+    '.pdf': 'Documentos',
+
+    '.png': 'Imagens',
+    '.jpg': 'Imagens',
+
+    '.mp3': 'Músicas e Sons',
+    '.mp4': 'Vídeos',
+
+    '.zip': 'ZIPs'
+}
 
 while True:
     time.sleep(1)
