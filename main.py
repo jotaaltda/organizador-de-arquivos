b0908ac2ccac8e32c939a4060b570f3bcf752234
Joao Paulo <jotape.hin@gmail.com>
2026-06-14 19:44:47 -0300

    Adiciona impressão do tipo do arquivo e importa colorama

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import time, os
 
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
+from colorama import Fore, init
+
+init(autoreset=True)
 
 class monitora_arquivos(FileSystemEventHandler):
     def on_created(self, event):
         nome_arquivo = os.path.basename(event.src_path)
-        print(f'Arquivo adicionado: {nome_arquivo}')
+        tipo_arquivo = os.path.splitext(nome_arquivo)
+        print(f'Arquivo adicionado: {Fore.YELLOW + nome_arquivo}, extensão: {Fore.YELLOW + tipo_arquivo[1]}')
 
 observador = Observer()
 monitor = monitora_arquivos()
