4fa1641efecdd79cd1039231787db1709f6c9842
Joao Paulo <jotape.hin@gmail.com>
2026-06-14 20:25:47 -0300

    Implementa organização dos arquivos via shutil.move

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time, os
+import time, os, shutil
 
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
@@ -10,15 +10,22 @@ class monitora_arquivos(FileSystemEventHandler):
     def on_created(self, event):
         nome_arquivo = os.path.basename(event.src_path)
         nome, extensao = os.path.splitext(nome_arquivo)
-        categoria = tipo_arquivo[extensao]
-        print(f'Arquivo adicionado: {Fore.YELLOW + nome_arquivo + Style.RESET_ALL}. Extensão: {Fore.YELLOW + extensao + Style.RESET_ALL}, categoria: {Fore.YELLOW + categoria + Style.RESET_ALL}')
+
+        try:
+            categoria = tipo_arquivo[extensao]
+        except KeyError:
+            categoria = 'Outros'
+
+        print(f'Arquivo adicionado: {Fore.YELLOW + nome_arquivo + Style.RESET_ALL}, extensão: {Fore.YELLOW + extensao + Style.RESET_ALL}')
+
+        destino = os.path.join(r'C:\Users\jotap\OneDrive\Área de Trabalho\Arquivos', categoria, nome_arquivo)
+
+        shutil.move(event.src_path, destino, copy_function=shutil.copy2)
 
 observador = Observer()
 monitor = monitora_arquivos()
 
 observador.schedule(monitor, r'C:\Users\jotap\OneDrive\Área de Trabalho\Arquivos', recursive=False)
-        
-observador.start()
 
 tipo_arquivo = {
     '.txt': 'Documentos',
@@ -30,8 +37,12 @@ tipo_arquivo = {
     '.mp3': 'Músicas e Sons',
     '.mp4': 'Vídeos',
 
-    '.zip': 'ZIPs'
+    '.zip': 'ZIPs',
+
+    '.exe': 'Programas'
 }
+
+observador.start()
 
 while True:
     time.sleep(1)
